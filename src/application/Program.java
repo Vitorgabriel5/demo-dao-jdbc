eb514bb6002fde65d166f60124ac1826b5ef0016
Vitor Gabriel <156606648+Vitorgabriel5@users.noreply.github.com>
2025-10-31 16:54:42 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,20 @@
 package application;
 
 import model.entities.Deparment;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Deparment obj = new Deparment(1, "Books");
-        System.out.println(obj.toString());
+
+        Seller seller = new Seller(21, "John","john@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller.toString());
+
+
 
 
     }
