8d5ade9bfade5e25f2f9a908bcb2634ab0b3ec99
Vitor Gabriel <156606648+Vitorgabriel5@users.noreply.github.com>
2025-10-31 16:16:46 -0300

    Deparment entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,13 @@
 package application;
 
+import model.entities.Deparment;
+
 public class Program {
     public static void main(String[] args) {
+
+        Deparment obj = new Deparment(1, "Books");
+        System.out.println(obj.toString());
+
 
     }
 }
---
+++ src/model/entities/Deparment.java
@@ -0,0 +1,53 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Deparment implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+
+     public Deparment(){
+     }
+
+    public Deparment(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Deparment deparment = (Deparment) o;
+        return Objects.equals(getId(), deparment.getId()) && Objects.equals(getName(), deparment.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Deparment " + "id= " + id + ", name= " + name + '\'';
+    }
+}
